d799803840af48a82c8259684c28f313538febbe
Sukanya <gantasalasaisukanya@gmail.com>
2023-04-19 19:07:41 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+b=int(input("enter a number"))
+h=int(input("enter a number"))
+s=int(input("enter a number"))
+l=int(input("enter a number"))
+area_t=0.5*b*h
+area_sq=s*s
+area_r=l*b
+print(area_t)
+print(area_sq)
+print(area_r)
